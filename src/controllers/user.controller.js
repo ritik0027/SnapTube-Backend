97be770ca87906d95cfeab070496f32bf25a250c
Ritik  Kumar <rsengar0089@gmail.com>
2024-08-05 23:29:51 +0530

    added refreshAccessToken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiResponse} from '../utils/ApiResponse.js';
 import {ApiError} from "../utils/ApiError.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { User} from "../models/user.model.js"
+import jwt from "jsonwebtoken"
 
 
 
