340daf456532288bc60dced598f303fd5ee59363
Ritik  Kumar <rsengar0089@gmail.com>
2024-08-22 23:52:57 +0530

    added subscription controller and its routes

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,18 +8,118 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    // TODO: toggle subscription
-})
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400,"Cannot find the channel")
+    }
 
-// controller to return subscriber list of a channel
+    const channel=await User.findById(channelId) 
+    if(!channel){
+        throw new ApiError(404,"Channel does not exist")
+    }
+
+    const user = await User.findOne({
+        refreshToken: req.cookies.refreshToken,
+    })
+    
+    if (!user) {
+        throw new ApiError(404, "Subscriber not found")
+    }
+
+
+    const userSub=await Subscription.findOne({
+        subscriber: user._id,
+        channel: channelId,
+    });
+
+    //if user is subscribed- unsubscribe 
+    if(userSub){
+        const unsubscribe= await Subscription.findOneAndDelete(
+            {
+                subscriber: user._id,
+                channel: channel._id
+            }
+        )
+
+        if (!unsubscribe) {
+            throw new ApiError(500, "Something went wrong while unsubscribing ");
+        }
+
+        return(
+            res
+            .status(200)
+            .json(new ApiResponse(200,unsubscribe,"User unsubscribed"))
+        )
+    }
+
+    //else subscribe the channel
+    if(!userSub){
+        const subscribe=await Subscription.create(
+            {
+                subscriber: user._id,
+                channel: channel._id
+            }
+        )
+        if (!subscribe) {
+            throw new ApiError(500,"Error while subscribing the channel")
+        }
+        return(
+            res
+            .status(200)
+            .json(new ApiResponse(200,subscribe,"User subscribed"))
+        )
+    }
+});
+
+
+
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-})
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400,"Cannot fetch channel id from params")
+    }
 
-// controller to return channel list to which user has subscribed
+    const channel=await User.findById(channelId)
+    if(!channel){
+        throw new ApiError(404,"Channel does not exist")
+    }
+
+
+    const subscribers=await Subscription.find(
+        {
+            channel:channel?._id
+        }
+    ).populate('subscriber')
+
+    
+    const subscriberCount=await Subscription.countDocuments({
+        channel:channelId
+    })
+
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200,{subscriberCount,subscribers},"Subscribers retrieved successfully"))
+    )
+});
+
+
+
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
-})
+
+    const subscriptions = await Subscription.find({ subscriber: subscriberId }).populate('channel');
+
+    const subscriptionsCount=await Subscription.countDocuments({
+        subscriber: subscriberId    
+    })
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200,{subscriptionsCount,subscriptions},"Subscribed channels fetched successfully"))
+    )
+});
 
 export {
     toggleSubscription,
