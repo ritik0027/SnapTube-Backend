7483140c5c4c3c45c1c8207934a62b789f45377b
Ritik  Kumar <rsengar0089@gmail.com>
2024-08-24 00:18:51 +0530

    added dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 import {Video} from "../models/video.model.js"
+import { User } from "../models/user.model.js"
 import {Subscription} from "../models/subscription.model.js"
 import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
@@ -7,11 +8,78 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const user=await User.findOne({
+        refreshToken:req.cookies.refreshToken
+    })
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    const totalVideoViews=await Video.aggregate([
+        {
+            $match:{
+                owner:user._id
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalViews:{
+                    $sum:"$views"
+                }
+            }
+        }
+    ])
+
+
+    const totalLikes=await Like.countDocuments({
+        likedBy:user._id
+    })
+
+    const totalVideos = await Video.countDocuments({
+        owner: user._id 
+    })
+
+    const totalSubscribers = await Subscription.countDocuments({
+        channel: user._id 
+    })
+
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200,{
+            totalVideoViews: totalVideoViews[0]?.totalViews || 0,
+            totalLikes,
+            totalSubscribers,
+            totalVideos
+        }))
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    const user= await User.findOne({
+        refreshToken: req.cookies.refreshToken
+    })
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    const videos=await Video.find({
+        owner:user?._id
+    })
+
+    if (!videos || videos.length === 0) {
+        throw new ApiError(500, "Error while fetching the videos")
+    }
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200,videos,"Videos fetched successfully"))
+    )
 })
 
 export {
